6377eca2706610af034cb73d5b5ae648bc157b44
David Zeng <castinglok@gmail.com>
2021-08-09 11:15:19 -0400

    new functionality to compile overall stats

--- AutoRefresh.py
+++ AutoRefresh.py
@@ -1,9 +1,18 @@
+from compileStats import updateTotals
 from pyautogui import *
 import pyautogui
 import time
+import datetime
 import keyboard
-import random
 import win32api, win32con
+
+#starting time
+startDT = datetime.datetime.now()
+
+#counters
+cov_Counter = 0
+mystic_Counter = 0
+SSCounter = 0
 
 def click(x,y):
     win32api.SetCursorPos((x,y))
@@ -11,14 +20,39 @@ def click(x,y):
     time.sleep(0.4)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
+def writeLog(totalStats):
+    try:
+        print("Attempting to open 'temp.log'")
+        file = open("temp.log", "x")
+        print("File does not exist, creating 'temp.log'")
+        file.write(totalStats[2:])
+    except:
+        print("File already exists, appending to file instead")
+        file = open("temp.log", "a")
+        file.write(totalStats)
+    finally:
+        file.close()
+
+def printStats():
+    currDT = datetime.datetime.now()
+
+    totalStats = "\n\n----------BOOKMARKS BOUGHT------------\n"
+    totalStats += currDT.strftime("%x") + " " + currDT.strftime("%X") + "\n"
+    totalStats += "Elapsed Time: " + str(currDT - startDT) + "\n"
+    totalStats += "Covenant Bookmarks: " + str(cov_Counter) + "\n"
+    totalStats += "Mystic Bookmarks: " + str(mystic_Counter) + "\n"
+    totalStats += "SkyStones Spent: " + str(SSCounter) + "\n"
+    totalStats += "--------------------------------------"
+
+    writeLog(totalStats)
+    print(totalStats[1:])
+
 click(1810,14)
 
 #pyautogui.scroll(amount_to_scroll, x=moveToX, y=moveToY)
 #Scroll at semi random place
 #Aprox place X: 1263 Y:  590
-cov_Counter = 0
-mystic_Counter = 0
-SSCounter = 0
+
 
 while keyboard.is_pressed('q') == False:
     
@@ -26,99 +60,118 @@ while keyboard.is_pressed('q') == False:
 #The confidence is added due to little variations in the background
     Coven_pos=pyautogui.locateOnScreen('covenant.png',confidence=0.95)
     Mystic_pos=pyautogui.locateOnScreen('mystic.png',confidence=0.95)
-    time.sleep(0.3)
+    time.sleep(0.5)
 #Checks for covenant
     if (Coven_pos) != None:
-        time.sleep(0.3)
+        time.sleep(0.5)
         print("Buy Covenant Summons.")
-        Coven_point=pyautogui.center(Coven_pos)
-        click(Coven_point[0]+800, Coven_point[1]+50)
-        click(Coven_point[0]+800, Coven_point[1]+50)
-        time.sleep(0.3)#wait for confirm button
+        try:
+            Coven_point=pyautogui.center(Coven_pos)
+            click(Coven_point[0]+800, Coven_point[1]+50)
+            click(Coven_point[0]+800, Coven_point[1]+50)
+        except:
+            printStats()
+        time.sleep(0.5)#wait for confirm button
         Buy_button_Covenant_pos=pyautogui.locateOnScreen('Buy_button_Covenant.png')
         Buy_button_Covenant_point=pyautogui.center(Buy_button_Covenant_pos)
         click(Buy_button_Covenant_point[0], Buy_button_Covenant_point[1])
         click(Buy_button_Covenant_point[0], Buy_button_Covenant_point[1])
         cov_Counter += 5
-        time.sleep(0.3)
+        time.sleep(0.5)
     else:
         print("No Covenant summons to buy.")       
 
 #checks for mystic
     if (Mystic_pos) != None:
-        time.sleep(0.3)        
+        time.sleep(0.5)        
         print("Buy Mystic Summons.")
-        Mystic_point=pyautogui.center(Mystic_pos)
-        click(Mystic_point[0]+800, Mystic_point[1]+50)
-        click(Mystic_point[0]+800, Mystic_point[1]+50)
-        time.sleep(0.3)#wait for confirm button
+        try:
+            Mystic_point=pyautogui.center(Mystic_pos)
+            click(Mystic_point[0]+800, Mystic_point[1]+50)
+            click(Mystic_point[0]+800, Mystic_point[1]+50)
+        except:
+            printStats()
+        time.sleep(0.5)#wait for confirm button
         Buy_button_Mystic_pos=pyautogui.locateOnScreen('Buy_button_Mystic.png')
         Buy_button_Mystic_point=pyautogui.center(Buy_button_Mystic_pos)
         click(Buy_button_Mystic_point[0], Buy_button_Mystic_point[1])
         click(Buy_button_Mystic_point[0], Buy_button_Mystic_point[1])
         mystic_Counter += 50
-        time.sleep(0.3)
+        time.sleep(0.5)
     else:
-        time.sleep(0.3)
+        time.sleep(0.5)
         print("No Mystic summons to buy.")
         click(x=1263,y=590)
-
+    
     pyautogui.scroll(-5, x=1263, y=590)
-    time.sleep(3)
-    click(x=1263,y=590)
+    time.sleep(1)
     Coven_pos2=pyautogui.locateOnScreen('covenant.png',confidence=0.95)
     Mystic_pos2=pyautogui.locateOnScreen('mystic.png',confidence=0.95)
 	
+#break point check to close macro
+    if(keyboard.is_pressed('q')):
+        break   
+
 #Checks for covenant
     if (Coven_pos2) != None:
-        time.sleep(0.3)
+        time.sleep(0.5)
         print("Buy Covenant Summons.")
-        Coven_point=pyautogui.center(Coven_pos2)
-        click(Coven_point[0]+800, Coven_point[1]+50)
-        click(Coven_point[0]+800, Coven_point[1]+50)
-        time.sleep(0.3)#wait for confirm button
+        try:
+            Coven_point=pyautogui.center(Coven_pos2)
+            click(Coven_point[0]+800, Coven_point[1]+50)
+            click(Coven_point[0]+800, Coven_point[1]+50)
+        except:
+            printStats()
+        time.sleep(0.5)#wait for confirm button
         Buy_button_Covenant_pos=pyautogui.locateOnScreen('Buy_button_Covenant.png')
         Buy_button_Covenant_point=pyautogui.center(Buy_button_Covenant_pos)
         click(Buy_button_Covenant_point[0], Buy_button_Covenant_point[1])
         click(Buy_button_Covenant_point[0], Buy_button_Covenant_point[1])
         cov_Counter += 5
-        time.sleep(0.3)
+        time.sleep(0.5)
     else:
         print("No Covenant summons to buy.")
         
 #checks for mystic
     if (Mystic_pos2) != None:
-        time.sleep(0.3)        
+        time.sleep(0.5)        
         print("Buy Mystic Summons.")
-        Mystic_point=pyautogui.center(Mystic_pos2)
-        click(Mystic_point[0]+800, Mystic_point[1]+50)
-        click(Mystic_point[0]+800, Mystic_point[1]+50)
-        time.sleep(0.3)#wait for confirm button
+        try:
+            Mystic_point=pyautogui.center(Mystic_pos2)
+            click(Mystic_point[0]+800, Mystic_point[1]+50)
+            click(Mystic_point[0]+800, Mystic_point[1]+50)
+        except:
+            printStats()
+        time.sleep(0.5)#wait for confirm button
         Buy_button_Mystic_pos=pyautogui.locateOnScreen('Buy_button_Mystic.png')
         Buy_button_Mystic_point=pyautogui.center(Buy_button_Mystic_pos)
         click(Buy_button_Mystic_point[0], Buy_button_Mystic_point[1])
         click(Buy_button_Mystic_point[0], Buy_button_Mystic_point[1])
         mystic_Counter += 50
-        time.sleep(0.3)
+        time.sleep(0.5)
     else:
         print("No Mystic summons to buy.")
         click(x=1263,y=590)
-        
+
+#break point check to close macro
+    if(keyboard.is_pressed('q')):
+        break    
+    
 #Finally refreshes
     RB_point=pyautogui.center(RB_pos)
     click(RB_point[0], RB_point[1])
     click(RB_point[0], RB_point[1])
-    time.sleep(0.3)#wait for confirm to appear
+    time.sleep(0.5)#wait for confirm to appear
     Confirm_pos=pyautogui.locateOnScreen('confirm button.png')
-    Confirm_point=pyautogui.center(Confirm_pos)
-    click(Confirm_point[0], Confirm_point[1])
-    click(Confirm_point[0], Confirm_point[1])
+    try:
+        Confirm_point=pyautogui.center(Confirm_pos)
+        click(Confirm_point[0], Confirm_point[1])
+        click(Confirm_point[0], Confirm_point[1])
+    except:
+        printStats()
     SSCounter += 3
-    time.sleep(0.3)
+    time.sleep(0.5)
 
 #print stats at end
-print("-------------TOTAL SUMMONS------------")
-print("Covenant Bookmarks: " + str(cov_Counter))
-print("Mystic Bookmarks: " + str(mystic_Counter))
-print("SkyStones Spent: " + str(SSCounter))
-print("--------------------------------------")
+printStats()
+updateTotals()
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # E7-Auto-Shop-Refresh-Custom
 This is a custom E7-Auto-Shop-Refresh, the original is made by EmaOlay : https://github.com/EmaOlay/E7-Auto-Shop-Refresh
 
+Sleep timers may also need to be adjusted as the speed that each PC can run E7 will differ
 PNG files may need to be updated as the pixels on each PC may differ
